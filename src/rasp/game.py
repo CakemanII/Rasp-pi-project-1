8f01d59845f21595620b0f9e0a696acca6037249
CakemanII <tyler.m.jundt@icloud.com>
2025-11-20 22:57:00 -0500

    removed print statement in time

--- src/rasp/game.py
+++ src/rasp/game.py
@@ -158,7 +158,6 @@ class ColorGame:
         previousTime = datetime.datetime.now()
         while not self.isGameOver() or self._gameStarted == False:
             time.sleep(UPDATE_TIME_DELAY)
-            print(self._elapsingTime )
             if self._elapsingTime == False:
                 continue
 
