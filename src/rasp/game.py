317cdba63f318cacafb43d20395f0f0a19c2f0cb
CakemanII <tyler.m.jundt@icloud.com>
2025-11-20 23:55:15 -0500

    fix24

--- src/rasp/game.py
+++ src/rasp/game.py
@@ -102,10 +102,10 @@ class ColorGame:
         # Check if the input is correct
         targetColor = self._correctColors[self._correctInputCount]
         if selectedColor == targetColor:
-            threading.Thread(self._correctInput).start()
+            threading.Thread(target=self._correctInput, daemon=False).start()
             return "success"
         else:
-            threading.Thread(self._incorrectInput).start()
+            threading.Thread(target=self._incorrectInput, daemon=False).start()
             return "wrong"
 
 
